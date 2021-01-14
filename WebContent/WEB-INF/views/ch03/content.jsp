91c852fe849353b5c212410537b5014d61626c63
sungbeom90 <sungbeom90@gmail.com>
2021-01-14 10:27:49 +0900

    소스 수정

--- WebContent/WEB-INF/views/ch03/content.jsp
+++ WebContent/WEB-INF/views/ch03/content.jsp
@@ -25,8 +25,35 @@
 				<div class="content">
 					
 					<div class="sector">
-						<h5>요청 매핑</h5>
+						<h5>GET방식 데이터 전달</h5>
 						<div>
+							<a class="btn btn-info btn-sm" href="method1?param1=문자열&param2=5&param3=3.14&param4=true&param5=2021-01-14">데이터 전달</a>
+						</div>
+					</div>
+					
+					<div class="sector">
+						<h5>POST 방식 데이터 전달</h5>
+						<div>
+							<form method="post" action="method1">
+								<input type="text" name="param1" value="문자열"/><br/>
+								<input type="number" name="param2" value="5"/><br/>
+								<input type="number" name="param3" value="3.14"/><br/>
+								<input type="checkbox" name="param4" checked/><br/>
+								<input type="date" name="param5" value="2020-12-25"/><br/>
+								<button class="btn btn-info btn-sm">데이터 전달</button>
+							</form>
+						</div>
+					</div>
+					
+					<div class="sector">
+						<h5>디폴트 값</h5>
+						<div>
+							<form method="post" action="method3">
+								<input type="text" name="param1" value="snowman"/><br/>
+								<input type="number" name="param2" value="5"/><br/>
+								<input type="number" name="param3" value="3.14"/><br/>
+								<button class="btn btn-info btn-sm">데이터 전달</button>
+							</form>
 						</div>
 					</div>
 					
--- src/main/java/com/mycompany/webapp/controller/Ch03Controller.java
+++ src/main/java/com/mycompany/webapp/controller/Ch03Controller.java
@@ -1,10 +1,15 @@
 package com.mycompany.webapp.controller;
 
+import java.util.Date;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 @RequestMapping("/ch03")
@@ -15,6 +20,43 @@ public class Ch03Controller {
 	@GetMapping("/content")
 	public String content() {
 		logger.info("실행");
+		return "ch03/content";
+	}
+	
+	@RequestMapping("/method1")
+	public String method1(String param1, int param2, double param3, boolean param4, @DateTimeFormat(pattern="yyyy-MM-dd") Date param5) {
+		logger.info("param1:"+param1);
+		logger.info("param2:"+param2);
+		logger.info("param3:"+param3);
+		logger.info("param4:"+param4);
+		logger.info("param5:"+param5);
+		return "ch03/content";
+	}
+	
+	@RequestMapping("/method2")
+	public String method2(
+			@RequestParam("param1") String arg1, 
+			@RequestParam("param2") int arg2, 
+			@RequestParam("param3") double arg3, 
+			@RequestParam("param4") boolean arg4, 
+			@DateTimeFormat(pattern="yyyy-MM-dd") Date param5) {
+		logger.info("param1:"+arg1);
+		logger.info("param2:"+arg2);
+		logger.info("param3:"+arg3);
+		logger.info("param4:"+arg4);
+		logger.info("param5:"+param5);
+		return "ch03/content";
+	}
+	@RequestMapping("/method3")
+	public String method3(
+			String param1, 
+			@RequestParam(defaultValue="0") int param2, 
+			@RequestParam(defaultValue="0.0") double param3, 
+			@RequestParam(defaultValue="false") boolean param4) {
+		logger.info("param1:"+param1);
+		logger.info("param2:"+param2);
+		logger.info("param3:"+param3);
+		logger.info("param4:"+param4);
 		return "ch03/content";
 	}
 }
