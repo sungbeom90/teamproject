1f7c26f59ed57ea8c5b6d25d7639fe40de181085
sungb <sungb@DESKTOP-SAMUEL>
2021-01-17 03:18:28 +0900

    홈서비스 추가

---
+++ src/main/java/com/mycompany/teamproject/controller/HomeService.java
@@ -0,0 +1,14 @@
+package com.mycompany.teamproject.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Service
+public class HomeService {
+	private static final Logger logger=LoggerFactory.getLogger(HomeService.class);
+	
+	
+}
