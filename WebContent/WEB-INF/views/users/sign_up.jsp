294564b5b1db263fa26bedbcf40a048f0b161c58
sungbeom90 <sungbeom90@gmail.com>
2021-01-18 20:28:28 +0900

    회원가입 개선

--- WebContent/WEB-INF/views/users/sign_up.jsp
+++ WebContent/WEB-INF/views/users/sign_up.jsp
@@ -25,12 +25,6 @@
 				<jsp:include page="/WEB-INF/views/include/menu.jsp"/>
 				<div class="content container">					
 					<div class="sector">
-						<h1>users 필드</h1>
-						<h2>sign_up 필드</h2>
-						<h2>ch07 또는 ch08을 이용하여 회원가입 구현</h2>
-						<h2>객체를 이용해서 데이터를 받아보자</h2>
-						<h2>이전에 작성했던 회원가입 방식을 적용해보자(비밀번호 확인, 빈란 알려주기)</h2>
-						<h2>가입 이후에는 ch06 리다이렉트를 적용해보자</h2>
 						<h2>회원가입</h2>
 					
 							<form method="post" enctype="multipart/form-data" action="sign_up_success">
--- src/main/java/com/mycompany/teamproject/controller/UsersController.java
+++ src/main/java/com/mycompany/teamproject/controller/UsersController.java
@@ -4,9 +4,9 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
@@ -20,13 +20,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.mycompany.teamproject.dto.Ch09User;
 import com.mycompany.teamproject.dto.UserDto;
+import com.mycompany.teamproject.service.UsersService;
 
 @Controller
 @RequestMapping("/users")
 public class UsersController {
 	private static final Logger logger= LoggerFactory.getLogger(UsersController.class);
+	
+	@Resource
+	UsersService usersService;
 	
 	// http://.../teamproject/users 생략됨
 			@RequestMapping("/content")		
@@ -47,50 +50,8 @@ public class UsersController {
 				return "users/sign_up";
 			}
 			@PostMapping("/sign_up_success")
-			public String photoUpload(UserDto userDto) {
-				//문자파트 정보얻기
-				String uemail = userDto.getUemail();
-				String upassword = userDto.getUpassword();
-				String uname = userDto.getUname();
-				String usex = userDto.getUsex();
-				String uagree_e1 = userDto.getUagree_e1();
-				String uagree_e2 = userDto.getUagree_e2();
-				String uagree_o1 = userDto.getUagree_o1();
-				String uagree_o2 = userDto.getUagree_o2();
-				
-				
-				logger.info("uemail: "+uemail);
-				logger.info("upassword: "+upassword);
-				logger.info("uname: "+uname);
-				logger.info("usex: "+usex);
-				logger.info("uagree_o2: "+uagree_e1);
-				logger.info("uagree_o2: "+uagree_e2);
-				logger.info("uagree_o2: "+uagree_o1);
-				logger.info("uagree_o2: "+uagree_o2);
-				
-				
-				
-				//파일파트 정보얻기
-				MultipartFile uphoto= userDto.getUphoto();
-				if(!uphoto.isEmpty()) {
-					String originalFileName = uphoto.getOriginalFilename();
-					String contentType = uphoto.getContentType();
-					long size = uphoto.getSize();
-					logger.info("originalFileName: "+originalFileName);
-					logger.info("contentType: "+contentType);
-					logger.info("size: "+size);
-					
-					//파일 저장 이름 및 경로
-					String saveDirPath = "D:/MyWorkspace/uploadfiles/";
-					String fileName = new Date().getTime() + "-" + originalFileName;
-					String filePath = saveDirPath + fileName;
-					File file = new File(filePath);
-					try {
-						uphoto.transferTo(file);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
-				}
+			public String sign_up_success(UserDto userDto, HttpSession session) {
+				usersService.sign_up_success(userDto);
 				logger.info("회원가입 완료");
 				return "redirect:/users/sign_in";
 			}
