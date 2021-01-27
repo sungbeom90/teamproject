c327de6975364c9db0ba8122b361a388f4339f23
sungbeom90 <sungbeom90@gmail.com>
2021-01-27 11:47:04 +0900

    연결테스트 완료

--- src/main/java/com/mycompany/teamproject/controller/MainController.java
+++ src/main/java/com/mycompany/teamproject/controller/MainController.java
@@ -1,8 +1,15 @@
 package com.mycompany.teamproject.controller;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,6 +23,26 @@ public class MainController {
 		@RequestMapping("/content")
 		public String content() {
 			logger.info("실행");
+			return "main/content";
+		}
+		
+		@Resource
+		private DataSource dataSource;
+		
+		
+		
+		@GetMapping("/conntest")
+		public String conntest() {
+			try {
+				//커넥션 풀에서 커넥션 대여
+				Connection conn= dataSource.getConnection();
+				logger.info("연결성공");
+				//대여한 커넥션 반납
+				conn.close();
+			} catch (SQLException e) {
+				logger.info("연결실패");
+				e.printStackTrace();
+			}
 			return "main/content";
 		}
 		
