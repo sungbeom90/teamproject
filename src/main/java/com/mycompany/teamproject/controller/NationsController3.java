a4d603adb67696e963b95bc8f6830cdb8433efb7
sungbeom90 <sungbeom90@gmail.com>
2021-01-27 20:59:34 +0900

    nation 사진띄우기

--- src/main/java/com/mycompany/teamproject/controller/NationsController3.java
+++ src/main/java/com/mycompany/teamproject/controller/NationsController3.java
@@ -17,10 +17,10 @@ import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.mycompany.teamproject.dto.CountryDto;
 import com.mycompany.teamproject.dto.Nation;
 import com.mycompany.teamproject.service.NationService;
-import com.mycompany.webapp.dto.Ch14Member;
+
+
 
 @Controller
 @RequestMapping("/nations3")
@@ -54,6 +54,42 @@ public class NationsController3 {
 			model.addAttribute("nation1", nation);
 			return "nations/nationlist3";
 		}
+		
+		@GetMapping("/nationread")
+		public String nationread(int nation_id, Model model) {
+			logger.info("실행");
+			Nation nation = nationService.getNation(nation_id);
+			model.addAttribute("nation", nation);
+			return "nations/nationlist3";
+		}
+		
+		@GetMapping("/nimage")
+		public void mphoto(int nation_id, HttpSession session, HttpServletResponse response) throws Exception{
+			
+			Nation nation = nationService.getNation(nation_id);
+			String filePath=null;
+			if(nation.getNimagesname() !=null) {   ///첨부파일이 있을때
+				String nimagesname = nation.getNimagesname();
+				String nname = nation.getNname();
+				filePath = "D:/MyWorkspace/teatfiles/nations/" + nname + "/" + nimagesname;			
+				response.setContentType(nation.getNimagetype()); //받아올 사진타입을 받아오기 위함.
+				
+				String nimageoname = nation.getNimageoname();
+				nimageoname=new String(nimageoname.getBytes("UTF-8"), "ISO-8859-1");  // 한글 파일명을 살리기 위함.
+				response.setHeader("Content-Disposition", "attachment; filename=\""+nimageoname+"\"");  //원본이름으로 주기위함
+				
+			} else {							// 첨부파일이 없을때
+				filePath = "D:/MyWorkspace/teatfiles/nations/defaultnimage.jpg";		
+				response.setContentType("image/jpg");			
+			}
+			OutputStream os= response.getOutputStream();
+			InputStream is = new FileInputStream(filePath);
+			
+			FileCopyUtils.copy(is, os);
+			os.flush();
+			os.close();
+			is.close();
+			}
 		
 		
 		
