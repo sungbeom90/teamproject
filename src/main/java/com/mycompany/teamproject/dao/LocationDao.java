01a37e3220cdd5dd5f41f78eca04921acbe3dfa3
sungbeom90 <sungbeom90@gmail.com>
2021-02-01 20:54:54 +0900

    임폴트

--- src/main/java/com/mycompany/teamproject/dao/LocationDao.java
+++ src/main/java/com/mycompany/teamproject/dao/LocationDao.java
@@ -9,10 +9,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Repository;
 
-import com.mycompany.teamproject.controller.RegisterController;
 import com.mycompany.teamproject.dto.LocationDto;
 import com.mycompany.teamproject.dto.LocationPager;
-import com.mycompany.teamproject.dto.NationDto;
 
 
 @Repository
